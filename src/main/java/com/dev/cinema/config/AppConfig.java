74130730a785bed72274fad9a52f92ef04193eed
Mikhail771 <mmihajlov771@gmail.com>
2020-10-22 16:04:09 +0300

    changed method for packages scan

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -1,12 +1,5 @@
 package com.dev.cinema.config;
 
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.Movie;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.Order;
-import com.dev.cinema.model.ShoppingCart;
-import com.dev.cinema.model.Ticket;
-import com.dev.cinema.model.User;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -49,8 +42,7 @@ public class AppConfig {
         properties.put("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(properties);
-        factoryBean.setAnnotatedClasses(CinemaHall.class, Movie.class, MovieSession.class,
-                Order.class, ShoppingCart.class, Ticket.class, User.class);
+        factoryBean.setPackagesToScan("com.dev.cinema.model");
         return factoryBean;
     }
 }
